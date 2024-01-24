c3efb00026f18cd78cbd1bd2e43f7cfc074189fa
muffindud <ccatlabuga2002@gmail.com>
2024-01-24 16:38:56 +0200

    add required fields to the Register page

--- screens/Register.js
+++ screens/Register.js
@@ -10,25 +10,35 @@ import {
   Button,
 } from "react-native";
 import { styles } from "../styles/Styles";
-import BackButton from "../components/BackButton";
+import { postSignup } from "../components/Requests";
 
 export default function Register({ navigation }) {
-  const [username, setUsername] = useState("");
-  const [emailOrPhone, setEmailOrPhone] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
   const [password, setPassword] = useState("");
   const [passwordVisibility, setPasswordVisibility] = useState(false);
+  const [refreshing, setRefreshing] = useState(true);
 
   const handleSignUp = () => {
     // Add your logic for handling sign up here
     // You can use the values of username, emailOrPhone, and password
     // to make API calls or perform other necessary actions
     // For now, let's just log the values
-    console.log("Username:", username);
-    console.log("Email or Phone:", emailOrPhone);
+    console.log("First Name:", firstName);
+    console.log("Last Name:", lastName);
+    console.log("Email:", email);
+    console.log("Phone:", phone);
     console.log("Password:", password);
 
+    if (refreshing) {
+      setRefreshing(false);
+      postSignup(firstName, lastName, email, phone, password);
+    }
+
     // Navigate to MainScreen and pass the username as a parameter
-    navigation.navigate("MainScreen", { userName: username });
+    navigation.navigate("MainScreen");
   };
 
   return (
@@ -43,15 +53,27 @@ export default function Register({ navigation }) {
           <Text style={styles.register_signin_header}>Welcome</Text>
           <TextInput
             style={styles.input_field}
-            value={username}
-            onChangeText={(newUsername) => setUsername(newUsername)}
-            placeholder="Username"
+            value={firstName}
+            onChangeText={(newFirstName) => setFirstName(newFirstName)}
+            placeholder="First Name"
           />
           <TextInput
-            value={emailOrPhone}
-            onChangeText={(newEmailOrPhone) => setEmailOrPhone(newEmailOrPhone)}
             style={styles.input_field}
-            placeholder="Phone or Email"
+            value={lastName}
+            onChangeText={(newLastName) => setLastName(newLastName)}
+            placeholder="Last Name"
+          />
+          <TextInput
+            value={email}
+            onChangeText={(newEmail) => setEmail(newEmail)}
+            style={styles.input_field}
+            placeholder="Email Address"
+          />
+          <TextInput
+            value={phone}
+            onChangeText={(newPhone) => setPhone(newPhone)}
+            style={styles.input_field}
+            placeholder="Phone Number"
           />
           <TextInput
             value={password}
